6c33e1c9ca801bf27480f62ad036749ff3f8a33c
Zonghao Huang <kyle0923@qq.com>
2024-03-19 23:48:49 -0400

    use emoji instead of nerd-fonts

--- __init__.py
+++ __init__.py
@@ -3,26 +3,74 @@
 import fnmatch
 import os
 import stat
+import re
+import subprocess
 
 from ranger.api import register_linemode
 from ranger.core.linemode import LinemodeBase
 
 from .icons import file_node_extensions, file_node_exact_matches, file_node_pattern_matches
 
+def get_file_type(filename):
+    # Run the 'file' command and capture its output
+    result = subprocess.run(['file', filename], capture_output=True, text=True)
+
+    # Return the output of the 'file' command
+    return result.stdout.strip()
 
 def get_icon(file):
-    basename = os.path.basename(file.relative_path)
 
-    em_icon = file_node_exact_matches.get(basename.lower())
-    if em_icon is not None:
-        return em_icon
+    if file.is_link:
+        if not file.exists:
+            return '⚠️'
+        if file.stat and stat.S_ISDIR(file.stat.st_mode):
+            return '🔖'
+        return '🎯'
 
-    for pattern, pm_icon in file_node_pattern_matches.items():
-        if fnmatch.filter([basename], pattern):
-            return pm_icon
+    if file.is_socket:
+        return '💡'
 
-    default = '' if file.is_directory else ''
-    return file_node_extensions.get(file.extension, default)
+    if file.is_fifo:
+        return '🅿️'
+
+    if not file.is_directory and file.stat:
+        mode = file.stat.st_mode
+        if mode & stat.S_IXUSR:
+            return '🚀'
+        if stat.S_ISCHR(mode):
+            return '🖨️'
+        if stat.S_ISBLK(mode):
+            return '💾'
+
+    if file.is_directory:
+        return '📂'
+
+    filepath:str = file.relative_path
+    filename:str = os.path.basename(filepath)
+
+
+    source_file = ['.c', '.cpp', '.h', '.hpp', '.proto', 'makefile', 'Makefile', '.mk']
+
+    if re.search(r'[Dd]ocker', filename):
+        return '🐋'
+    if filename.endswith('.py'):
+        return '🐍'
+    if filename.endswith('.json') or filename.endswith('.yaml') or filename.endswith('.xml') or filename.endswith('.html'):
+        return '🪪'
+    if any(filename.endswith(ext) for ext in source_file):
+        return '✏️'
+
+    file_type:str = get_file_type(filepath).lower()
+    if filename.endswith('.sh') or filename.endswith('.bash') or re.search(r'shell', file_type):
+        return '🥷'
+    if re.search(r'elf', file_type):
+        return '👻'
+
+    if re.search(r'text', file_type):
+        return '📄'
+
+
+    return ' '
 
 
 def get_symbol(file):
@@ -48,8 +96,8 @@ def get_symbol(file):
         if stat.S_ISBLK(mode):
             return '+'
 
-    # if file.is_directory:
-    #     return '/'
+    if file.is_directory:
+        return '/'
 
     return ''
 
